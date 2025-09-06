8ea1cee5d622703297b1bf8866b7cb36df5bde26
Kashif <kashif147@gmail.com>
2025-09-06 12:05:19 +0100

    role scripts with mongodb connection sorted

--- config/db.js
+++ config/db.js
@@ -2,8 +2,15 @@ const mongoose = require("mongoose");
 
 exports.mongooseConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI).then((data) => {
-      console.log(`SuccessFully Connected to the MongoDB ===> "${data.connection.name}"`);
+    // Build connection string with environment variables
+    const mongoUri =
+      process.env.MONGO_URI ||
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@clusterprojectshell.tptnh8w.mongodb.net/${process.env.MONGO_DB}/?retryWrites=true&w=majority&appName=ClusterProjectShell`;
+
+    await mongoose.connect(mongoUri).then((data) => {
+      console.log(
+        `SuccessFully Connected to the MongoDB ===> "${data.connection.name}"`
+      );
     });
   } catch (e) {
     console.error("Unable to connect to database:", e);
